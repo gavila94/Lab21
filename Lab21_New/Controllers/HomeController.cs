1a4571502444dab6c2d391fdc061244fb6a50bdc
gavila94 <gabriellaravila@gmail.com>
2016-10-02 21:23:52 -0400

    Add files via upload

---
+++ Lab21_New/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Lab21_New.Models;
+
+namespace Lab21_New.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ViewResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult RegForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RegForm(RegistrationResponse registrationResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Thanks", registrationResponse);
+            } else
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Lab21_New/Models/RegistrationResponse.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lab21_New.Models
+{
+    public class RegistrationResponse
+
+    {
+        [Required(ErrorMessage = "Please specify your Title")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Please enter your first name")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Please enter your last name")]
+        public string LastName { get; set; }
+
+        public string Suffix { get; set; }
+
+        [Required(ErrorMessage = "Please enter your Address")]
+        public string Address { get; set; }
+
+        [Required(ErrorMessage = "Please enter your city")]
+        public string City { get; set;  }
+
+        [Required(ErrorMessage = "Please enter your state")]
+        public string State { get; set; }
+
+        [Required(ErrorMessage = "Please enter your zipcode")]
+        public float ZipCode { get; set; }
+
+        [Required(ErrorMessage = "Please enter your email address")]
+        [RegularExpression(".+\\@.+\\..+",
+            ErrorMessage = "Please enter a valid email address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Please enter your phone number")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Please enter your birthday")]
+        public string DOB { get; set; }
+
+        [Required(ErrorMessage = "Please specify your gender")]
+        public string Gender { get; set; } 
+
+    }
+}
